e47dbbff7deff9f99e14d8a739d9001523598eab
akriticode09 <158453972+akriticode09@users.noreply.github.com>
2024-11-12 18:16:04 +0530

    printing 1 dimensional array

---
+++ arrayprinting.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a[5],n,i;
+    printf("enter the value of n");
+    scanf("%d",& n);
+    printf("enter the elemenys of array");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
